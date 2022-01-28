e347ca0e33d9e743a5c8cb1e2c978f816963ebe0
Karthikey Saxena <karthikey.saxena@rksv.in>
2022-01-28 11:43:23 +0530

    Converted to TypeScript

--- src/service/todoService.ts
+++ src/service/todoService.ts
@@ -3,27 +3,26 @@ import { ITodo, ITodos } from "../interfaces/interface";
 
 const backendUrl = "http://localhost:8080/todos"
 
-export const getAll = async() => {
+export const getAll = async(): Promise<ITodos[]> => {
     const response = await axios.get(backendUrl);
     return response.data;
 }
 
-export const getById = async(id: number) => {
+export const getById = async(id: number): Promise<ITodos> => {
     const response = await axios.get(backendUrl + `/${id}`);
     return response.data;
 }
 
-export const add = async(data: ITodo) => {
+export const add = async(data: ITodo): Promise<ITodos> => {
     const response = await axios.post(backendUrl, data);
     return response.data;
 }
 
-export const update = async(data: ITodos) => {
+export const update = async(data: ITodos): Promise<ITodos> => {
     const response = await axios.put(backendUrl, data);
     return response.data;
 }
 
-export const deleteItem = async(id: number) => {
-    const response = await axios.delete(backendUrl + `?id=${id}`);
-    return response.data;
+export const deleteItem = async(id: number): Promise<void> => {
+    await axios.delete(backendUrl + `?id=${id}`);
 }
